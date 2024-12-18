1f5c0dc38cb8ec925f006dafd4e215a18e40409f
Sebas <sperezb@uteq.edu.ec>
2024-12-18 15:46:33 -0500

    Logo para al aplicación

--- expo/urls.py
+++ expo/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.urls import include
 urlpatterns = [
