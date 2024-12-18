18541f4331c620b2fe18317d3ce9689f88184ea7
Sebas <sperezb@uteq.edu.ec>
2024-12-18 03:02:33 -0500

    fix

--- libreria/models.py
+++ libreria/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.forms import ModelForm  
 # Create your models here.
 class Libro(models.Model):
-    id= models.AutoField(primary_key=True) 
+    id= models.AutoField(primary_key=True)
     nombre=models.CharField(max_length=30, verbose_name='Titulo')
     imagenes= models.ImageField(upload_to='Images', verbose_name='Imagen',null=True)
     descripcion= models.TextField(verbose_name='Descripcion',null=True)
-
- 
