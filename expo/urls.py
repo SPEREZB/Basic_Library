ec44937b7c4a9d18a15d21094cf74399e30faa50
Sebas <sperezb@uteq.edu.ec>
2024-12-19 09:15:41 -0500

    Creación de ruta para carpeta media

--- expo/urls.py
+++ expo/urls.py
@@ -18,7 +18,13 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('libreria.urls')), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
